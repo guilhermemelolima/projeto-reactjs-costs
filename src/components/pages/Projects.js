b0182e9489fa1bc9effdb8a707b4208aba72b18e
Guilherme Melo <limaguilherme.melo@gmail.com>
2024-08-12 18:58:20 -0300

    Criação de uma página para listar todos os projetos

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,103 @@
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
+import Loading from "../layout/Loading";
+
+import styles from "./Projects.module.css";
+
+function Projects() {
+	const [projects, setProjects] = useState([]);
+	const [removeLoading, setRemoveLoading] = useState(false);
+	const [projectMessage, setProjectMessage] = useState("");
+
+	const location = useLocation();
+	let message = "";
+	if (location.state) {
+		message = location.state.message;
+	}
+
+	// fazendo o loader aprecer de forma forçada usando o Timeout
+	useEffect(() => {
+		setTimeout(() => {
+			fetch("http://localhost:5000/projects", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+				.then((resp) => resp.json())
+				.then((data) => {
+					setProjects(data);
+					setRemoveLoading(true);
+				})
+				.catch((err) => console.log(err));
+		}, 300);
+	}, []);
+
+	// fazendo o loader aparecer de acordo com o tempo da API
+	// useEffect(() => {
+	//   fetch('http://localhost:5000/project',{
+	//     method: 'GET',
+	//     headers: {
+	//       'Content-Type': 'application/json',
+	//     },
+	//   })
+	//   .then((resp) => resp.json())
+	//   .then((data) => {
+	//     setProjects(data)
+	//     setRemoveLoading(true)
+	//   })
+	//   .catch((err) => console.log(err))
+	// }, [])
+
+	function removeProject(id) {
+		fetch(`http://localhost:5000/projects/${id}`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log(resp);
+				console.log(resp.json());
+			})
+			.then(() => {
+				setProjects(projects.filter((project) => project.id !== id));
+				setProjectMessage("Projeto removido com sucesso!");
+			});
+	}
+
+	return (
+		<div className={styles.project_conteiner}>
+			<div className={styles.title_container}>
+				<h1>Meus Projetos</h1>
+				<LinkButton to="/new-project" text="Criar Projeto" />
+			</div>
+			{message && <Message type="success" msg={message} />}
+			{projectMessage && <Message type="success" msg={projectMessage} />}
+			<Container customClass="start">
+				{projects.length > 0 &&
+					projects.map((project) => (
+						<ProjectCard
+							id={project.id}
+							name={project.name}
+							budget={project.budget}
+							category={project.category.name}
+							key={project.id}
+							handleRemove={removeProject}
+						/>
+					))}
+				{!removeLoading && <Loading />}
+				{removeLoading && projects.length === 0 && (
+					<p>Não há projetos cadastrados!</p>
+				)}
+			</Container>
+		</div>
+	);
+}
+
+export default Projects;
