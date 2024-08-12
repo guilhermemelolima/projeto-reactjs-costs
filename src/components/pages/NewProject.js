efe0125829ebfba24206658fba75b7511ac06915
Guilherme Melo <limaguilherme.melo@gmail.com>
2024-08-12 19:01:28 -0300

    Atualização na URL de consulta da API

---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+
+import ProjectForm from "../project/ProjectForm";
+import style from "./NewProject.module.css";
+
+function NewProject() {
+	const navigate = useNavigate();
+
+	function createPost(project) {
+		// initialize and services
+		project.cost = 0;
+		project.services = [];
+
+		fetch("http://localhost:5000/projects", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(project),
+		})
+			.then((resp) => resp.json())
+			.then((data) => {
+				const state = { message: "Projeto criado com sucesso!" };
+				navigate("/projects", { state });
+			})
+			.catch((err) => console.log(err));
+	}
+
+	return (
+		<section className={style.newProjectContainer}>
+			<h1>Criar projeto</h1>
+			<p>Crie seu projeto para depois adicionar os serviços</p>
+			<ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+		</section>
+	);
+}
+
+export default NewProject;
