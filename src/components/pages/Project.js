7224c36e36ed25dcb234eed49d60a1ca1e07b0d4
Guilherme Melo <limaguilherme.melo@gmail.com>
2024-08-12 18:56:46 -0300

    Criação de uma página para a edição de um projeto e para a adição de serviços

---
+++ src/components/pages/Project.js
@@ -0,0 +1,122 @@
+import styles from "./Project.module.css";
+
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Loading from "../layout/Loading";
+import Container from "../layout/Container";
+import ProjectForm from "../project/ProjectForm";
+import Message from "../layout/Message";
+
+function Project() {
+	const { id } = useParams();
+	const [project, setProject] = useState([]);
+	const [showProjectForm, setShowProjectForm] = useState(false);
+	const [message, setMessage] = useState("");
+	const [type, setType] = useState("");
+
+	useEffect(() => {
+		setTimeout(() => {
+			fetch(`http://localhost:5000/projects/${id}`, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+				.then((resp) => resp.json())
+				.then((data) => setProject(data))
+				.catch((err) => console.log(err));
+		}, 300);
+	}, [id]);
+
+	// useEffect(() => {
+	// 	fetch(`http://localhost:5000/project/${id}`, {
+	// 		method: "GET",
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 	})
+	// 		.then((resp) => resp.json())
+	// 		.then((data) => setProject(data))
+	// 		.catch((err) => console.log(err));
+	// }, [id]);
+
+	function toggleProjectForm() {
+		setShowProjectForm(!showProjectForm);
+	}
+
+	function editPost(project) {
+		if (project.budget < project.cost) {
+			setMessage(
+				"O Orçamento não pode ser menor que o custo do projeto!"
+			);
+			setType("error");
+			return false;
+		}
+
+		fetch(`http://localhost:5000/projects/${project.id}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(project),
+		})
+			.then((resp) => resp.json())
+			.then((data) => {
+				setProject(data);
+				setShowProjectForm(!showProjectForm);
+				setMessage("Projeto atualizado!");
+				setType("success");
+			})
+			.catch((err) => console.log(err));
+	}
+
+	return (
+		<>
+			{project.name ? (
+				<div className={styles.project_details}>
+					<Container customClass="column">
+						{message && <Message type={type} msg={message} />}
+						<div className={styles.details_container}>
+							<h1>Projeto: {project.name}</h1>
+							<button
+								onClick={toggleProjectForm}
+								className={styles.btn}
+							>
+								{!showProjectForm ? "Editar projeto" : "Fechar"}
+							</button>
+							{!showProjectForm ? (
+								<div className={styles.project_info}>
+									<p>
+										<span>Categoria:</span>
+										{project.category.name}
+									</p>
+									<p>
+										<span>Total Orçamento:</span> R$
+										{project.budget}
+									</p>
+									<p>
+										<span>Total Utilitários:</span> R$
+										{project.cost}
+									</p>
+								</div>
+							) : (
+								<div className={styles.project_info}>
+									<ProjectForm
+										handleSubmit={editPost}
+										btnText="Concluir edição"
+										projectData={project}
+									/>
+								</div>
+							)}
+						</div>
+					</Container>
+				</div>
+			) : (
+				<Loading />
+			)}
+		</>
+	);
+}
+
+export default Project;
