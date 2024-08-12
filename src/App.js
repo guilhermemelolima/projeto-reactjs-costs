bd190177d7ffd2d2368a7c416af5f9d0a0151ea2
Guilherme Melo <limaguilherme.melo@gmail.com>
2024-08-12 18:51:38 -0300

    Adicionada nova rota

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // IMPORTS
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // PAGINAS
-import Home from './components/pages/Home'
-import Empresa from './components/pages/Empresa'
-import Contato from './components/pages/Contato'
-import Projeto from './components/pages/Projeto'
+import Home from "./components/pages/Home";
+import Empresa from "./components/pages/Empresa";
+import Contato from "./components/pages/Contato";
+import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
 
 // LAYOUT
-import Container from './components/layout/Container'
-import NavBar from './components/layout/NavBar'
-import Footer from './components/layout/Footer'
-import NovoProjeto from './components/pages/NovoProjeto'
-
+import Container from "./components/layout/Container";
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
+import NewProject from "./components/pages/NewProject";
 
 /*  PALETA
     #222831
@@ -24,42 +24,64 @@ import NovoProjeto from './components/pages/NovoProjeto'
 */
 
 function App() {
+	return (
+		<Router>
+			<NavBar />
 
-  return (
-    <Router>
-        <NavBar />
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Container customClass="min_height">
+							<Home />
+						</Container>
+					}
+				/>
+				<Route
+					path="/empresa"
+					element={
+						<Container customClass="min_height">
+							<Empresa />
+						</Container>
+					}
+				/>
+				<Route
+					path="/contato"
+					element={
+						<Container customClass="min_height">
+							<Contato />
+						</Container>
+					}
+				/>
+				<Route
+					path="/project/:id"
+					element={
+						<Container customClass="min_height">
+							<Project />
+						</Container>
+					}
+				/>
+				<Route
+					path="/projects"
+					element={
+						<Container customClass="min_height">
+							<Projects />
+						</Container>
+					}
+				/>
+				<Route
+					path="/new-project"
+					element={
+						<Container customClass="min_height">
+							<NewProject />
+						</Container>
+					}
+				/>
+			</Routes>
 
-        <Routes>
-            <Route path='/' element={
-                <Container customClass="min_height">
-                    <Home />
-                </Container>
-            } />
-            <Route path='/empresa' element={
-                <Container customClass="min_height">
-                    <Empresa />
-                </Container>
-            } />
-            <Route path='/contato' element={
-                <Container customClass="min_height">
-                    <Contato />
-                </Container>
-            } />
-            <Route path='/projeto' element={
-                <Container customClass="min_height">
-                    <Projeto />
-                </Container>}
-            />
-            <Route path='/novo-projeto' element={
-                <Container customClass="min_height">
-                    <NovoProjeto />
-                </Container>}
-            />
-        </Routes>
-
-        <Footer />
-    </Router>
-  );
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
